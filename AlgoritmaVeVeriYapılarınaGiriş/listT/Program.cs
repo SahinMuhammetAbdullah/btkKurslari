9032a19caa98f93a7acbc25ed98d4390bf0356bf
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2023-12-03 18:18:25 +0300

    main

--- AlgoritmaVeVeriYapılarınaGiriş/listT/Program.cs
+++ AlgoritmaVeVeriYapılarınaGiriş/listT/Program.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 namespace ListT
 {
+    public class Araba<T> : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var list = new List<int>();
         }
     }
 }
